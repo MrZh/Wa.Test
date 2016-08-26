4e413fee9f43074634005e1fea8a0cccd61e056f
Lucifer <Lucifer@SEVEN-PC>
2016-08-26 17:15:24 +0800

    添加项目文件。

---
+++ Wa.Test/CommonEncrypt.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Wa.Test
+{
+    public class CommonEncrypt
+    {
+        /// <summary>
+        /// 密钥
+        /// </summary>
+        private static readonly string pkey = "smkldospdosldaaa";//key，可自行修改
+
+        /// <summary>
+        /// 偏移量
+        /// </summary>
+        private static readonly string piv = "0392039203920300"; //偏移量,可自行修改
+
+        /// <summary>
+        /// 根据默认密钥和偏移量加密
+        /// </summary>
+        /// <param name="toEncrypt">源字符串</param>
+        /// <returns>密码</returns>
+        public static string Encrypt(string toEncrypt)
+        {
+            var key = pkey;
+            var iv = piv;
+            var keyArray = Encoding.UTF8.GetBytes(key);
+            var ivArray = Encoding.UTF8.GetBytes(iv);
+            var toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+            var rDel = new RijndaelManaged
+            {
+                BlockSize = 128,
+                KeySize = 256,
+                FeedbackSize = 128,
+                Padding = PaddingMode.PKCS7,
+                Key = keyArray,
+                IV = ivArray,
+                Mode = CipherMode.CBC
+            };
+
+            var cTransform = rDel.CreateEncryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        
+        /// <summary>
+        /// 根据密钥和偏移量加密
+        /// </summary>
+        /// <param name="toEncrypt">源字符串</param>
+        /// <param name="key">密钥</param>
+        /// <param name="iv">偏移量</param>
+        /// <returns>密码</returns>
+        public static string Encrypt(string toEncrypt, string key, string iv)
+        {
+            var keyArray = Encoding.UTF8.GetBytes(key);
+            var ivArray = Encoding.UTF8.GetBytes(iv);
+            var toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+            var rDel = new RijndaelManaged
+            {
+                BlockSize = 128,
+                KeySize = 256,
+                FeedbackSize = 128,
+                Padding = PaddingMode.PKCS7,
+                Key = keyArray,
+                IV = ivArray,
+                Mode = CipherMode.CBC
+            };
+
+            var cTransform = rDel.CreateEncryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        /// <summary>
+        /// 根据默认密钥和偏移量解密
+        /// </summary>
+        /// <param name="toDecrypt">密码</param>
+        /// <returns>源字符串</returns>
+        public static string Decrypt(string toDecrypt)
+        {
+            var key = pkey;
+            var iv = piv;
+            var keyArray = Encoding.UTF8.GetBytes(key);
+            var ivArray = Encoding.UTF8.GetBytes(iv);
+            var toEncryptArray = Convert.FromBase64String(toDecrypt);
+            var rDel = new RijndaelManaged
+            {
+                BlockSize = 128,
+                KeySize = 256,
+                FeedbackSize = 128,
+                Padding = PaddingMode.PKCS7,
+                Key = keyArray,
+                IV = ivArray,
+                Mode = CipherMode.CBC
+            };
+
+            var cTransform = rDel.CreateDecryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            return Encoding.UTF8.GetString(resultArray);
+        }
+
+        /// <summary>
+        /// 根据密钥和偏移量解密
+        /// </summary>
+        /// <param name="toDecrypt">密码</param>
+        /// <param name="key">密钥</param>
+        /// <param name="iv">偏移量</param>
+        /// <returns>源字符串</returns>
+        public static string Decrypt(string toDecrypt, string key, string iv)
+        {
+            var keyArray = Encoding.UTF8.GetBytes(key);
+            var ivArray = Encoding.UTF8.GetBytes(iv);
+            var toEncryptArray = Convert.FromBase64String(toDecrypt);
+            var rDel = new RijndaelManaged
+            {
+                BlockSize = 128,
+                KeySize = 256,
+                FeedbackSize = 128,
+                Padding = PaddingMode.PKCS7,
+                Key = keyArray,
+                IV = ivArray,
+                Mode = CipherMode.CBC
+            };
+
+            var cTransform = rDel.CreateDecryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
---
+++ Wa.Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Wa.Test
+{
+    public class Program
+    {
+       // private static string pkey = "smkldospdosldaaa";//key，可自行修改
+       // private static string piv = "0392039203920300"; //偏移量,可自行修改
+        public static void Main(string[] args)
+        {
+            var encrytpData =CommonEncrypt.Encrypt("abc");
+            Console.WriteLine(encrytpData);
+
+            var decryptData = CommonEncrypt.Decrypt("gR2in2uzMOZcftlFQm8tDw==");
+            Console.WriteLine(decryptData);
+
+            Console.ReadLine();
+        }
+    }
+}
